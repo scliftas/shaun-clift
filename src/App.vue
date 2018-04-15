1a446998d4ad5b3e061e8dfd14047388566dca23
scliftas <scliftas@gmail.com>
2018-04-15 03:12:16 +0100

    Added navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
     <fullPage>
+      <navbar/>
+
       <fullSection>
         Test
       </fullSection>
+      
       <fullSection>
         Test 2
       </fullSection>
@@ -14,13 +17,19 @@
 <script>
 import fullPage from './components/fullPage/fullPage.vue'
 import fullSection from './components/fullPage/fullSection.vue'
+import navbar from './components/elements/navbar.vue'
 
 export default {
   name: 'app',
 
   components: {
     fullPage,
-    fullSection
+    fullSection,
+    navbar
   }
 }
 </script>
+
+<style>
+@import 'assets/css/styles.css';
+</style>
---
+++ src/components/elements/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-navbar toggleable sticky>
+        <b-navbar-brand href="#">
+            <span class="title">Shaun Clift</span>
+            <br/>
+            <span class="subtitle">Web Developer</span>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+
+        <b-collapse is-nav id="nav_dropdown_collapse">
+            
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="#">About</b-nav-item>
+                <b-nav-item href="#">Skills</b-nav-item>
+                <b-nav-item href="#">Experience</b-nav-item>
+                <b-nav-item href="#">Contact</b-nav-item>
+            </b-navbar-nav>
+
+        </b-collapse>
+    </b-navbar>
+</template>
