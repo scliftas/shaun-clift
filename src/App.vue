756738710615e2d250e4a6dffb37255c71228dd9
scliftas <scliftas@gmail.com>
2018-04-16 23:37:55 +0100

    Moved styling into their respective components

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,15 @@
       </fullSection>
 
       <fullSection id="skills">
+        <skills/>
       </fullSection>
 
       <fullSection id="experience">
+        <experience/>
       </fullSection>
 
       <fullSection id="contact">
+        <contact/>
       </fullSection>
     </fullPage>
   </div>
@@ -31,6 +34,9 @@ import fullSection from './components/fullPage/fullSection.vue'
 import navbar from './components/elements/navbar.vue'
 import home from './components/sections/home.vue'
 import about from './components/sections/about.vue'
+import skills from './components/sections/skills.vue'
+import experience from './components/sections/experience.vue'
+import contact from './components/sections/contact.vue'
 
 export default {
   name: 'app',
@@ -40,18 +46,48 @@ export default {
     fullSection,
     navbar,
     home,
-    about
+    about,
+    skills,
+    experience,
+    contact
   }
 }
 </script>
 
 <style>
-@import 'assets/css/styles.css';
-
+/* animation */
 .v-enter-active, .v-leave-active {
   transition: opacity 2s;
 }
 .v-enter, .v-leave-to {
   opacity: 0;
+}
+
+/* general */
+#title, .nav-link, h1, h3, p {
+    color: #FFFFFF !important;
+}
+
+#subtitle, .nav-link:hover, h2 {
+    color: #868686 !important;
+}
+
+/* sections */
+.section {
+    text-align: center;
+    padding-top: 5em;
+    padding-bottom: 5em;
+    padding-left: 2.5em;
+    padding-right: 2.5em;
+}
+
+.section-child {
+    height: 100%;
+}
+
+.vertical-align {
+    position: relative;
+    top: 50%;
+    transform: translateY(-50%);
 }
 </style>
