801156057580e834eba1fa2fdd4ef8fa229b6955
scliftas <scliftas@gmail.com>
2018-04-23 11:56:17 +0100

    Separated about paragraphs

--- src/components/sections/about.vue
+++ src/components/sections/about.vue
@@ -10,9 +10,10 @@
                     </b-col>
 
                     <b-col id="about-text" md="12" lg="6">
-                        <p class="vertical-align">
+                        <p>
                             Bacon ipsum dolor amet beef pork cupim chicken. Beef corned beef t-bone porchetta kevin bacon. Drumstick bresaola meatloaf swine t-bone short ribs hamburger. Picanha venison andouille, kielbasa biltong ball tip kevin strip steak jerky beef pork belly short ribs bresaola ham hock sirloin. Biltong tongue landjaeger brisket, strip steak shank rump bresaola alcatra short loin salami ribeye. Alcatra bacon pork loin, tenderloin tri-tip shank pork drumstick.
-                            <br/><br/>
+                        </p>
+                        <p>
                             Shank beef buffalo spare ribs turkey kevin ball tip capicola. Short loin capicola bacon ham hock pig prosciutto strip steak venison drumstick rump ball tip tail leberkas tongue meatloaf. Sausage filet mignon jerky cow swine, short ribs alcatra spare ribs fatback pork pancetta pork belly pork loin shoulder tenderloin. Short ribs pork belly rump doner, short loin bacon salami cow ball tip porchetta beef ribs strip steak t-bone sirloin flank.
                         </p>
                     </b-col>
